b915210ff898fd44cd55f7027383056ffc3fac25
Julius <juliusniemeyer1995@gmail.com>
2024-08-17 16:16:23 +0200

    a

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import 
 
-def check_for_unimportant_things_like_colleges_or_hotels(pd_series : pd.Series):
-    match
+def check_if_unimportant_things_like_colleges_or_hotels(pd_series : pd.Series) -> bool:
+    sitelinks = pd_series['sitelinks']
+    match pd_series['thingLabel']:
+        case 'hotel' if sitelinks < 10:
+            return True
+        case 'college' if sitelinks < 10:
+            return True
+        case _:
+            return False
