411d13af9f6aa5dc7bef21c57af7116b95959105
Julius <juliusniemeyer1995@gmail.com>
2024-08-17 18:19:02 +0200

    get_data is class based now

--- src/get_data.py
+++ src/get_data.py
@@ -1,45 +1,42 @@
 import requests
 import pandas as pd
 
-bangalore_coordinates_string: str = '''"Point(77.55273 12.98313)"'''
+class wikidata_fetcher():
+    def __init__(self, sparql_file_path: str, lat : float, lon : float) -> None:
+        self.lat = lat
+        self.lon = lon
+        with open(sparql_file_path, 'r') as f:
+            self.query = f.read()
 
-with open('queries/bangalore_query.sparql', 'r') as f:
-    query = f.read()
-print(query)
+    def get_value(x, key):
+            try:
+                return x[key]['value']
+            except KeyError:
+                return ''
+     
+    def get_wikidata_table_out_of_location(self) -> pd.DataFrame:
+        old_coordinates_string: str = 'Point(77.55273 12.98313)'
+        new_coordinate_string = f'Point({self.lat} {self.lon})'
 
-url = "https://query.wikidata.org/sparql"
-headers = {
-    "User-Agent":
-    "geogame-image-fetcher/1.0 (juliusniemeyer1995@gmail.com) python requests"
-}
-r = requests.get(url,
-                 params={
-                     'format': 'json',
-                     'query': query
-                 },
-                 headers=headers)
-r = r.json()
-bindings_list = r['results']['bindings']
+        self.query = self.query.replace(old_coordinates_string,new_coordinate_string)
 
-def get_value(x, key):
-    try:
-        return x[key]['value']
-    except KeyError:
-        return ''
-df = pd.DataFrame()
-for key in bindings_list[0].keys():
-    print(key)
-    df[key] = [get_value(x,key) for x in bindings_list]
-df.to_csv('data/bengaluru_test.csv',index=False)
+        url = "https://query.wikidata.org/sparql"
+        headers = {
+            "User-Agent":
+            "geogame-image-fetcher/1.0 (juliusniemeyer1995@gmail.com) python requests"
+        }
+        r = requests.get(url,
+                        params={
+                            'format': 'json',
+                            'query': self.query
+                        },
+                        headers=headers)
+        r = r.json()
+        bindings_list = r['results']['bindings']
 
+        df = pd.DataFrame()
+        for key in bindings_list[0].keys():
+            print(key)
+            df[key] = [self.get_value(x,key) for x in bindings_list]
 
-# item_list = [x['itemLabel']['value'] for x in bindings_list]
-# article_en_list = [x['articleEN']['value'] for x in bindings_list]
-# article_de_list = [x['articleDE']['value'] for x in bindings_list]
-# lat_list = [x['lat']['value'] for x in bindings_list]
-# lon_list = [x['lon']['value'] for x in bindings_list]
-# sitelinks_list = [x['sitelinks']['value'] for x in bindings_list]
-# thing_list = [x['thingLabel']['value'] for x in bindings_list]
-
-
-# item,itemLabel,lat,lon,location,distance,sitelinks,thingLabel,thing,articleEN,articleDE
+        df.to_csv(f'data/{str(self.lat).replace('.','-')}_{str(self.lon).replace('.','-')}.csv',index=False)
