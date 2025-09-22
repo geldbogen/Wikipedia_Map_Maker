d5fb79a1a7e1306b4cab692fbed77d5c2b7286d5
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2025-09-22 22:43:50 +0200

    Update KmlHelper styles with new color codes
    - Changed style_S ID from 'icon-1739-E65100-normal' to 'icon-1739-FF0000-normal' for red color.
    - Updated style_map_S ID to match the new style_S ID.
    - Modified style_A ID from 'icon-1739-7CB342-normal' to 'icon-1739-FF8800-normal' for orange color.
    - Adjusted style_map_A ID to reflect the new style_A ID.
    - Altered style_B ID from 'icon-1739-9C27B0-normal' to 'icon-1739-FFFF00-normal' for yellow color.

--- src/kml_helper.py
+++ src/kml_helper.py
@@ -40,24 +40,24 @@ class KmlHelper():
         self.style_graves._id = 'icon-1556-FFD600-normal'
 
         self.style_S = simplekml.Style()
-        self.style_S._id = 'icon-1739-E65100-normal'
+        self.style_S._id = 'icon-1739-FF0000-normal'
         self.style_S.iconstyle.icon.href = 'https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png'
 
         # self.style_S.labelstyle = simplekml.LabelStyle(0)
 
         self.style_map_S = simplekml.StyleMap(normalstyle=self.style_S,)
-        self.style_map_S._id = 'icon-1739-E65100'
+        self.style_map_S._id = 'icon-1739-FF0000'
 
         self.style_A = simplekml.Style()
         self.style_A._id = 'icon-1739-7CB342-normal'
         self.style_A.iconstyle.icon.href = 'https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png'
 
         self.style_map_A = simplekml.StyleMap(normalstyle=self.style_A)
-        self.style_map_A._id = 'icon-1739-7CB342'
+        self.style_map_A._id = 'icon-1739-FF8800'
 
 
         self.style_B = simplekml.Style()
-        self.style_B._id = 'icon-1739-9C27B0-normal'
+        self.style_B._id = 'icon-1739-FFFF00-normal'
         self.style_B.iconstyle.icon.href = 'https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png'
 
         self.style_map_B = simplekml.StyleMap(normalstyle=self.style_B)
