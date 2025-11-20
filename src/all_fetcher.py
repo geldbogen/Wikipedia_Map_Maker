180c26f7731ceaedb9525962566b53fef437243b
Julius <juliusniemeyer1995@gmail.com>
2025-11-20 18:18:29 +0100

    a

--- src/all_fetcher.py
+++ src/all_fetcher.py
@@ -111,5 +111,5 @@ class AllFetcher():
         KmlHelper(self.place_name, final_df)
 
 if __name__ == '__main__':
-    my_all_fetcher = AllFetcher('Denver', 'united-states', distance=15, debug_mode=False)
+    my_all_fetcher = AllFetcher('Nuremberg', 'Germany', distance=15, debug_mode=False)
     my_all_fetcher.go()
